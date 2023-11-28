5800525aaa27b94a01225a8e4bddba705d57d679
Emilia <81705094+emiliaweb@users.noreply.github.com>
2023-11-28 22:21:58 +0600

    Playing with cookies

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const cookieParser = require('cookie-parser');
 
 const authRoutes = require('./routes/authRoutes');
 
@@ -12,6 +13,7 @@ dotenv.config();
 // middleware
 app.use(express.static('public'));
 app.use(express.json());
+app.use(cookieParser())
 
 // view engine
 app.set('view engine', 'ejs');
@@ -21,6 +23,21 @@ const dbURI = process.env.DB_URI;
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
   .then((result) => app.listen(4000))
   .catch((err) => console.log(err));
+
+// testing cookies
+app.get('/set-cookies', (req, res) => {
+  // res.setHeader('Set-Cookie', 'newUser=true');
+
+  res.cookie('newUser', false);
+  res.cookie('isEmployee', true, { maxAge: 1000 * 60 * 60 * 24, httpOnly: true });
+
+  res.send('you got a cookie');
+});
+
+
+app.get('/read-cookies', (req, res) => {
+  res.json(req.cookies)
+});
 
 // routes
 app.get('/', (req, res) => res.render('home'));
